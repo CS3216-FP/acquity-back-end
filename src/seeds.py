21b880cd702bc28eb1351f5ec61bbb01b7fbdae0
nwjbrandon <nwjbrandon@gmail.com>
2019-10-19 00:32:02 +0800

    update seeds

--- src/seeds.py
+++ src/seeds.py
@@ -1,6 +1,6 @@
 from passlib.hash import argon2
-
-from src.database import Security, User, session_scope
+import uuid
+from src.database import Security, User, session_scope, ChatRoom
 
 
 def seed_db():
@@ -15,6 +15,24 @@ def seed_db():
                     can_sell=True,
                 )
             )
+        if session.query(User).filter_by(email="b@b.com").count() == 0:
+            session.add(
+                User(
+                    email="b@b.com",
+                    hashed_password=argon2.hash("acquity"),
+                    full_name="Brandon",
+                    can_buy=True,
+                    can_sell=False,
+                )
+            )
+        brandon_id = getattr(session.query(User).filter_by(email="b@b.com").first(), "id")
+        ben_id = getattr(session.query(User).filter_by(email="a@a.com").first(), "id")
+        session.add(
+            ChatRoom(
+                seller_id=str(ben_id),
+                buyer_id=str(brandon_id),
+            )
+        )
         if session.query(Security).filter_by(name="Grab").count() == 0:
             session.add(Security(name="Grab"))
 
