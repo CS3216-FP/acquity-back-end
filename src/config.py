7e548a2e9d4eb405572d07178d9e99f27c1142a2
Herbert Ilhan Tanujaya <mail@herbert.id>
2019-10-21 16:56:54 +0800

    Add callback in the future via temporize

--- src/config.py
+++ src/config.py
@@ -16,11 +16,12 @@ APP_CONFIG = {
     "DATABASE_URL": getenv("DATABASE_URL", DEFAULT_DATABASE_URL),
     "CORS_AUTOMATIC_OPTIONS": True,
     "PORT": getenv("PORT", 8000),
+    "TEMPORIZE_URL": getenv("TEMPORIZE_URL"),
+    "TEMPORIZE_TOKEN": getenv("TEMPORIZE_TOKEN"),
     "SANIC_JWT_EXPIRATION_DELTA": 2 * 24 * 3600,  # 2 days
     "SANIC_JWT_USER_ID": "id",
     "SANIC_JWT_SECRET": getenv(
-        "JWT_SECRET",
-        "secret" if getenv("ACQUITY_ENV") in ["DEVELOPMENT", "TEST"] else "",
+        "JWT_SECRET", "secret" if ACQUITY_ENV in ["DEVELOPMENT", "TEST"] else ""
     ),
     "CLIENT_ID": getenv("CLIENT_ID"),
     "CLIENT_SECRET": getenv("CLIENT_SECRET"),
